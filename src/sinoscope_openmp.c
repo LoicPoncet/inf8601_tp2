f3245ea91a38311276b3f08bf6d91b60a3288a54
Loïc Poncet <lopon@l4712-11.info.polymtl.ca>
2018-11-05 17:08:06 -0500

    First good improvement thanks to OpenMP

--- src/sinoscope_openmp.c
+++ src/sinoscope_openmp.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <omp.h>
 
 #include "sinoscope.h"
 #include "color.h"
@@ -15,7 +16,7 @@
 
 int sinoscope_image_openmp(sinoscope_t *ptr)
 {
-    TODO("sinoscope_image_openmp");
+    //TODO("sinoscope_image_openmp");
 
     if (ptr == NULL)
         return -1;
@@ -23,23 +24,36 @@ int sinoscope_image_openmp(sinoscope_t *ptr)
     sinoscope_t sino = *ptr;
     int width = sino.width;
     int height = sino.height;
-    int x, y, index, taylor;
-    struct rgb c;
-    float val, px, py;
+    int x, y;
 
-    x = 1;
+    #pragma omp parallel for private(x)
     for (x = 1; x < width - 1; x++) {
-        for (y = 1; y < height - 1; y++) {
-            px = sino.dx * y - 2 * M_PI;
-            py = sino.dy * x - 2 * M_PI;
-            val = 0.0f;
-            for (taylor = 1; taylor <= sino.taylor; taylor += 2) {
-                val += sin(px * taylor * sino.phase1 + sino.time) / taylor + cos(py * taylor * sino.phase0) / taylor;
+        
+        int sino_taylor = sino.taylor;
+        float sino_phase0 = sino.phase0;
+        float sino_phase1 = sino.phase1;
+        int sino_interval = sino.interval;
+        float sino_interval_inv = sino.interval_inv;
+        float sino_dx = sino.dx;
+        float sino_dy = sino.dy;
+        float sino_time = sino.time;
+        struct rgb c;
+        int index, taylor;
+        
+        for (y = 1; y < height - 1; y++) {            
+            float px = sino_dx * y - 2 * M_PI;
+            float py = sino_dy * x - 2 * M_PI;
+
+            float val = 0.0f;
+            
+            for (taylor = 1; taylor <= sino_taylor; taylor += 2) {
+                val += sin(px * taylor * sino_phase1 + sino_time) / taylor + cos(py * taylor * sino_phase0) / taylor;
             }
             val = (atan(1.0 * val) - atan(-1.0 * val)) / (M_PI);
             val = (val + 1) * 100;
-            value_color(&c, val, sino.interval, sino.interval_inv);
-            index = (y * 3) + (x * 3) * sino.width;
+
+            value_color(&c, val, sino_interval, sino_interval_inv);
+            index = (y * 3) + (x * 3) * width;
             sino.buf[index + 0] = c.r;
             sino.buf[index + 1] = c.g;
             sino.buf[index + 2] = c.b;
